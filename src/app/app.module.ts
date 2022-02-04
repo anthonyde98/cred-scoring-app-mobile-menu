679713da6244be274fdd42acd4cae231af184cad
anthonyde98 <anthonyde98@gmail.com>
2022-02-04 15:45:57 -0400

    Acciones para incluir fingerprint,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, FingerprintAIO],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/config/config.page.ts
+++ src/app/pages/config/config.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Storage } from '@capacitor/storage';
 
-
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
+import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-config',
@@ -12,7 +13,7 @@ export class ConfigPage implements OnInit {
   actived: boolean;
   avaliable: boolean;
 
-  constructor() { }
+  constructor(private faio: FingerprintAIO, private toastr: ToastService) { }
 
   ngOnInit() {
     this.setButton()
@@ -20,15 +21,21 @@ export class ConfigPage implements OnInit {
   }
 
   async availableButton(){
-    
+    this.showFingerprintAuthDlg()
   }
 
   async setButton(){
     const { value } = await Storage.get({ key: 'credentials' });
     const credentials = JSON.parse(value || "0");
+    const sessionCredentials = {
+      username: sessionStorage.getItem('usuario'),
+      password: sessionStorage.getItem('contrasena')
+    }
 
-    if(credentials != "0")
+    if(credentials != "0" && (credentials.username == sessionCredentials.username && 
+      credentials.password == sessionCredentials.password)){
       this.actived = true;
+    }
     else
       this.actived = false;
   }
@@ -37,14 +44,16 @@ export class ConfigPage implements OnInit {
     if(!this.actived){
       this.actived = true;
 
-      const credentials = {
+      const sessionCredentials = {
         username: sessionStorage.getItem('usuario'),
         password: sessionStorage.getItem('contrasena')
       }
-  
+
+      await Storage.remove({ key: 'credentials' });
+
       await Storage.set({
         key: 'credentials',
-        value: JSON.stringify(credentials),
+        value: JSON.stringify(sessionCredentials),
       });
     }
     else{
@@ -52,5 +61,32 @@ export class ConfigPage implements OnInit {
 
       await Storage.remove({ key: 'credentials' });
     }
+  }
+
+  showFingerprintAuthDlg() {
+
+    this.faio.isAvailable().then((result: any) => {
+      this.toastr.toastr(result, "secondary", 5000);
+
+      this.faio.show({
+        cancelButtonTitle: 'Cancel',
+        description: "Some biometric description",
+        disableBackup: true,
+        title: 'Scanner Title',
+        fallbackButtonTitle: 'FB Back Button',
+        subtitle: 'This SubTitle'
+      })
+      .then((result: any) => {
+        console.log(result)
+        alert("Successfully Authenticated!")
+      })
+      .catch((error: any) => {
+        console.log(error)
+        alert("Match not found!")
+      });
+    })
+    .catch((error: any) => {
+      this.toastr.toastr(error, "danger", 5000);
+    });
   }
 }
